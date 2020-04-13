3102d8d4115733964b81be3c65675e1fdd94fde5
Nick Killin <nicholaskillin@gmail.com>
2020-04-13 13:52:16 -0400

    added Gloomhaven Card Manager to the list of messages

--- gloomhavenbot.js
+++ gloomhavenbot.js
@@ -20,11 +20,13 @@ client.on('message', message => {
     publicIpAddress.then(function(address) {
       message.channel.send(`Address: ${address}\nPort: 58888`);
     })
-  } else if(message.content.toLocaleLowerCase() === '!help') {
-    message.channel.send('Here is a list of the things you can ask me:\n\n"!market": This will give you the URL to our parties online town market\n"!helpersettings": This will give you the current ip address and port to use in Gloomhaven Helper.')
+  } else if(message.content.toLowerCase() === '!help') {
+    message.channel.send('Here is a list of the things you can ask me:\n\n"!market": This will give you the URL to our parties online town market\n"!helpersettings": This will give you the current ip address and port to use in Gloomhaven Helper.\n"!mycards": This will give you the URL for the app we use to manage our ability at attack modifier cards during the campaign.')
+  } else if(message.content.toLowerCase() === '!mycards') {
+    message.channel.send(" You can use the Gloomhaven Card Manager to manage your character's ability and attack modifier cards during the campaign.\nhttps://nicholaskillin.github.io/.")
   }
 });
 
 client.on('ready', () => {
   client.user.setPresence({activity: {name: '"!help" for help'}, status: "online"})
-})
+});
