7d2919c8fec3a963389e4406670a9f687f8a0dc3
Nick Killin <nicholaskillin@gmail.com>
2020-04-13 18:48:04 -0400

    putting error back

--- gloomhavenbot.js
+++ gloomhavenbot.js
@@ -50,11 +50,7 @@ function getNextSession(message) {
 
   // Load client secrets from a local file.
   fs.readFile('credentials.json', (err, content) => {
-    if (err) {
-      message.channel.send(`${err}`)
-    } 
-    
-    // return console.log('Error loading client secret file:', err);
+    if (err) return console.log('Error loading client secret file:', err);
     // Authorize a client with credentials, then call the Google Calendar API.
     authorize(JSON.parse(content), listEvents);
   });
