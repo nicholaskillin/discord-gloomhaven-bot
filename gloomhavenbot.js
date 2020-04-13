ef40d5c0b2f3f2b61b1e499d91f401409b653c10
Nick Killin <nicholaskillin@gmail.com>
2020-04-13 10:49:40 -0400

    added help and helpersettings commands

--- gloomhavenbot.js
+++ gloomhavenbot.js
@@ -1,15 +1,26 @@
 require('dotenv').config({path: '/home/pi/Desktop/gloomhavenBot/.env'})
+require('dotenv').config()
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = process.env.GLOOMHAVEN_BOT_TOKEN
+const publicIp = require('public-ip');
 
 client.login(token);
 
 client.on('message', message => {
   if(message.content.toLowerCase() === '!market') {
-    message.channel.send(`You can access the parties market here ${process.env.MARKET_URL}`)
-  } else if(message.content.toLowerCase() === 'marco') {
+    message.channel.send(`You can access the parties market here\n${process.env.MARKET_URL}`)
+  } else if(message.content.toLowerCase().includes('marco')) {
     message.channel.send(`polo`)
+  } else if(message.content.toLowerCase() === '!helpersettings') {
+    let publicIpAddress = (async () => {
+      return await publicIp.v4();
+    })();
+    publicIpAddress.then(function(address) {
+      message.channel.send(`Address: ${address}\nPort: 58888`);
+    })
+  } else if(message.content.toLocaleLowerCase() === '!help') {
+    message.channel.send('Here is a list of the things you can ask me:\n\n"!market": This will give you the URL to our parties online town market\n"!helpersettings": This will give you the current ip address and port to use in Gloomhaven Helper.')
   }
 });
