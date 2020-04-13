39ca930e3b97aefa4fbc202466b4290f251b0f86
Nick Killin <nicholaskillin@gmail.com>
2020-04-12 22:17:53 -0400

    add market url to secrets

--- gloomhavenbot.js
+++ gloomhavenbot.js
@@ -8,7 +8,7 @@ client.login(token);
 
 client.on('message', message => {
   if(message.content.toLowerCase() === '!market') {
-    message.channel.send(`You can access the parties market here https://heisch.github.io/gloomhaven-item-db/#eyJhbGwiOmZhbHNlLCJwcm9zcGVyaXR5IjozLCJpdGVtIjpbXSwiaXRlbXNJblVzZSI6eyIyIjoxLCI3IjoxLCIxMyI6MSwiMTUwIjoxfSwic29sb0NsYXNzIjpbIlNLIiwiQlQiXSwiZGlzY291bnQiOi0zLCJkaXNwbGF5QXMiOiJpbWFnZXMiLCJsb2NrU3BvaWxlclBhbmVsIjpmYWxzZSwiZW5hYmxlU3RvcmVTdG9ja01hbmFnZW1lbnQiOnRydWV9 `)
+    message.channel.send(`You can access the parties market here ${process.env.MARKET_URL}`)
   } else if(message.content.toLowerCase() === 'marco') {
     message.channel.send(`polo`)
   }
